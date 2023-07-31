5e26a8fc5099df8253c82c675496bc5c45cf9c9d
Michael Ellicott <mellic03@gmail.com>
2023-07-31 18:26:59 +1000

    README

--- src/main.c
+++ src/main.c
@@ -82,7 +82,7 @@ int main(int argc, char** argv)
   gameobject_translate(enemy, 5, -3, 0);
 
   GameObject *map = gameobject_create();
-  gameobject_assign_model(map, GE_model_load("src/assets/map1"));
+  gameobject_assign_model(map, GE_model_load("src/assets/benchmark"));
 
   player = player_create();
   player->game_object = gameobject_create();
